2ceb577d1221e5cf988ae18bc32003e6bb1eb393
rkthooriq <rkthooriq15@gmail.com>
2025-12-17 10:56:06 +0700

    Baikin menu dan main

--- main.cpp
+++ main.cpp
@@ -1,305 +1,41 @@
 #include <iostream>
 #include "header.h"
-#include "akun.h"
-#include <limits>
+#include "menu.h"
 
 using namespace std;
 
-void adminMenu(ListTurnamen &LT, ListAkun &LA);
-void userMenu(ListTurnamen &LT);
+int main() {
+    ListTurnamen L;
+    int pilih = 1;
+    createListTurnamen(L);
 
-// REGISTER
-
-void menuRegistrasi(ListAkun &LA) {
-    int pilih;
-
-    do {
-        cout << "\n===== REGISTER MENU =====\n";
-        cout << "1. Register Admin\n";
-        cout << "2. Register User\n";
-        cout << "0. Kembali\n";
-        cout << "Pilih: ";
+    while (pilih != 0) {
+        cout << "=================================================\n";
+        cout << "|  SISTEM MANAJEMEN TURNAMEN BADMINTON TAHUNAN  |\n";
+        cout << "=================================================\n";
+        cout << "| 1. Admin                                      |\n";
+        cout << "| 2. User                                       |\n";
+        cout << "| 0. Keluar                                     |\n";
+        cout << "=================================================\n";
+        cout << "Masukkan Pilihan Anda: ";
         cin >> pilih;
 
         if (pilih == 1) {
-            string usn, pw;
-            cout << "Username: ";
-            cin >> usn;
-            cout << "Password: ";
-            cin >> pw;
-            registerAdmin(LA, usn, pw);
+            menuAdmin(L);
         }
         else if (pilih == 2) {
-            string usn, pw;
-            cout << "Username: ";
-            cin >> usn;
-            cout << "Password: ";
-            cin >> pw;
-            registerUser(LA, usn, pw);
+            menuUser(L);
         }
         else if (pilih == 0) {
-            cout << "Kembali ke menu utama...\n";
+            cout << "Keluar dari program...\n";
         }
         else {
             cout << "Pilihan tidak valid!\n";
         }
 
-    } while (pilih != 0);
-}
-
-
-// LOGIN
-<<<<<<< HEAD
-adrAkun menuLogin(ListAkun &LA, ListTurnamen &LT) {
-=======
-
-void menuLogin(ListAkun &LA, ListTurnamen &LT) {
->>>>>>> c8aa98cce96ae9c3cba4b031bfab09002cbefa3d
-    string u, p;
-    cout << "\n=== LOGIN ===\n";
-    cout << "Username: "; cin >> u;
-    cout << "Password: "; cin >> p;
-
-    adrAkun acc = login(LA, u, p);
-
-    if (acc == nullptr) {
-        cout << ">> Login gagal! Username/password salah.\n";
-<<<<<<< HEAD
-        return nullptr;
-=======
-        return;
->>>>>>> c8aa98cce96ae9c3cba4b031bfab09002cbefa3d
+        cout << endl;
     }
 
-    cout << "\n>> Login sebagai [" << acc->role << "]\n";
-
-    if (acc->role == "admin")
-        adminMenu(LT, LA);
-    else
-        userMenu(LT);
-<<<<<<< HEAD
-
-    return acc;
-=======
->>>>>>> c8aa98cce96ae9c3cba4b031bfab09002cbefa3d
-}
-
-// ADMIN
-
-void adminMenu(ListTurnamen &LT, ListAkun &LA) {
-    int pilih;
-
-    do {
-        cout << "\n===== ADMIN MENU =====\n";
-        cout << "1. Tambah Turnamen\n";
-        cout << "2. Tampilkan Semua Turnamen\n";
-        cout << "3. Kelola Pemain dalam Turnamen\n";
-        cout << "0. Logout\n";
-        cout << "Pilih: ";
-        cin >> pilih;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        if (pilih == 1) {
-            Turnamen x;
-
-            cout << "\n=== INPUT DATA TURNAMEN ===\n";
-
-            cout << "Nama turnamen : ";
-            getline(cin, x.namaTurnamen);
-
-            cout << "Lokasi        : ";
-            getline(cin, x.lokasi);
-
-            cout << "Tanggal Mulai : ";
-            getline(cin, x.tanggalMulai);
-
-            cout << "Tanggal Selesai : ";
-            getline(cin, x.tanggalSelesai);
-
-            cout << "Tahun         : ";
-            cin >> x.tahun;
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-            cout << "Kategori      : ";
-            getline(cin, x.kategori);
-
-            adrTurnamen T = createElemenTurnamen(x);
-            insertLastTurnamen(LT, T);
-
-            cout << "\n>> Turnamen berhasil ditambahkan!\n";
-        }
-
-        else if (pilih == 2) {
-            displayTurnamen(LT);
-        }
-
-        else if (pilih == 3) {
-            string namaT;
-
-            cout << "Masukkan nama turnamen: ";
-            getline(cin, namaT);
-
-            adrTurnamen T = searchTurnamenByNama(LT, namaT);
-            if (!T) {
-                cout << ">> Turnamen tidak ditemukan!\n";
-            }
-            else {
-                int m;
-                do {
-                    cout << "\n=== KELOLA PEMAIN: " << T->info.namaTurnamen << " ===\n";
-                    cout << "1. Tambah Pemain (First)\n";
-                    cout << "2. Tambah Pemain (Last)\n";
-                    cout << "3. Hapus Pemain by ID\n";
-                    cout << "4. Cari Pemain\n";
-                    cout << "5. Tampilkan Semua Pemain\n";
-                    cout << "0. Kembali\n";
-                    cout << "Pilih: ";
-                    cin >> m;
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-                    if (m == 1 || m == 2) {
-                        string id, namap;
-                        int score;
-
-                        cout << "ID Pemain   : ";
-                        getline(cin, id);
-
-                        if (sudahIkutTahunIni(LT, id, T->info.tahun)) {
-                            cout << ">> Pemain sudah ikut turnamen lain tahun ini!\n";
-                            continue;
-                        }
-
-                        cout << "Nama Pemain : ";
-                        getline(cin, namap);
-
-                        cout << "Score       : ";
-                        cin >> score;
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-                        adrPemain P = createElmPemain(id, namap, score);
-
-                        if (m == 1) insertFirstPemain(T, P);
-                        else insertLastPemain(T, P);
-
-                        cout << ">> Pemain berhasil ditambahkan!\n";
-                    }
-
-                    else if (m == 3) {
-                        string id;
-                        cout << "ID pemain yang ingin dihapus: ";
-                        getline(cin, id);
-
-                        deletePemainByID(T, id);
-                    }
-
-                    else if (m == 4) {
-                        string id;
-                        cout << "ID Pemain: ";
-                        getline(cin, id);
-
-                        adrPemain P = findPemain(T, id);
-                        if (!P) cout << ">> Pemain tidak ditemukan!\n";
-                        else cout << "Nama: " << P->nama << ", Score: " << P->score << endl;
-                    }
-
-                    else if (m == 5) {
-                        printPemain(T);
-                    }
-
-                } while (m != 0);
-            }
-        }
-
-    } while (pilih != 0);
-}
-
-
-// USER
-void userMenu(ListTurnamen &LT) {
-    int pilih;
-
-    do {
-        cout << "\n===== USER MENU =====\n";
-        cout << "1. Lihat Semua Turnamen\n";
-        cout << "2. Cari Turnamen berdasarkan nama\n";
-        cout << "0. Logout\n";
-        cout << "Pilih: ";
-        cin >> pilih;
-        cin.ignore();
-
-        if (pilih == 1) {
-            displayTurnamen(LT);
-        }
-
-        else if (pilih == 2) {
-            string nama;
-            cout << "Nama turnamen: ";
-            getline(cin, nama);
-
-            adrTurnamen T = searchTurnamenByNama(LT, nama);
-            if (T == nullptr)
-                cout << ">> Tidak ditemukan!\n";
-            else {
-                cout << "\n--- DATA TURNAMEN ---\n";
-                cout << "Nama     : " << T->info.namaTurnamen << endl;
-                cout << "Lokasi   : " << T->info.lokasi << endl;
-                cout << "Tahun    : " << T->info.tahun << endl;
-                printPemain(T);
-            }
-        }
-
-    } while (pilih != 0);
-}
-
-
-int main() {
-    ListTurnamen LT;
-    ListAkun LA;
-
-    createListTurnamen(LT);
-    createAkunList(LA);
-
-    int pilih;
-
-    do {
-        cout << "\n===== MENU UTAMA =====\n";
-        cout << "1. Register\n";
-        cout << "2. Login\n";
-        cout << "0. Keluar\n";
-        cout << "Pilih: ";
-        cin >> pilih;
-        cin.ignore();
-
-        if (pilih == 1) {
-            menuRegistrasi(LA);
-        }
-
-        else if (pilih == 2) {
-<<<<<<< HEAD
-            adrAkun acc = menuLogin(LA, LT);
-=======
-            adrAkun acc = menuLogin(LA);
->>>>>>> c8aa98cce96ae9c3cba4b031bfab09002cbefa3d
-
-            if (acc != nullptr) {
-                if (acc->role == "admin")
-                    adminMenu(LT, LA);
-                else
-                    userMenu(LT);
-            }
-        }
-
-        else if (pilih != 0) {
-            cout << "Pilihan tidak valid!\n";
-        }
-
-    } while (pilih != 0);
-
-    cout << "Keluar...\n";
-<<<<<<< HEAD
-
-=======
->>>>>>> c8aa98cce96ae9c3cba4b031bfab09002cbefa3d
+    cout << "Program selesai.\n";
     return 0;
 }
-
---
+++ menu.cpp
@@ -0,0 +1,378 @@
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+
+void menuAdmin(ListTurnamen &L){
+    int pilih = 1;
+    while (pilih != 0){
+        cout <<"============================================"<<endl;
+        cout <<"|               MENU ADMIN                 |"<<endl;
+        cout <<"============================================"<<endl;
+        cout <<"|1. Menu Turnamen                          |"<<endl;
+        cout <<"|2. Menu Pemain                            |"<<endl;
+        cout <<"|0. Kembali ke main menu                   |"<<endl;
+        cout <<"============================================"<<endl;
+        cout << "Masukkan Pilihan Anda: ";
+        cin >> pilih;
+        if (pilih == 1){
+            menuTurnamen(L);
+        }else if (pilih == 2){
+            menuPemain(L);
+        }else if (pilih == 0){
+            cout<<"Kembali ke main menu ";
+        }else {
+            cout << "pilihan tidak valid ";
+        }
+        cout<<endl;
+    }
+}
+void menuTurnamen(ListTurnamen &L){
+    int pilih = 1;
+    Turnamen x;
+    adrTurnamen p, prec;
+    string nama;
+    int tahun;
+    while (pilih != 0) {
+        cout << "============================================"<<endl;
+        cout << "|              MENU TURNAMEN               |"<<endl;
+        cout << "============================================"<<endl;
+        cout << "| 1. Insert First Turnamen                 |"<<endl;
+        cout << "| 2. Insert Last Turnamen                  |"<<endl;
+        cout << "| 3. Insert After Turnamen                 |"<<endl;
+        cout << "| 4. Delete First Turnamen                 |"<<endl;
+        cout << "| 5. Delete Last Turnamen                  |"<<endl;
+        cout << "| 6. Delete After Turnamen                 |"<<endl;
+        cout << "| 7. Search Turnamen                       |"<<endl;
+        cout << "| 8. Print Semua Turnamen                  |"<<endl;
+        cout << "| 0. Kembali                               |"<<endl;
+        cout << "============================================"<<endl;
+        cout << "Masukkan Pilihan Anda: ";
+        cin >> pilih;
+        if (pilih == 1) {
+            inputDataTurnamen(x);
+            p = createElemenTurnamen(x);
+            insertFirstTurnamen(L, p);
+        }else if (pilih == 2) {
+            inputDataTurnamen(x);
+            p = createElemenTurnamen(x);
+            insertLastTurnamen(L, p);
+        }else if (pilih == 3){
+            cout <<"Insert setelah turnamen bernama : ";
+            cin >> nama;
+            prec = searchTurnamenByNama(L,nama);
+            if (prec != nullptr){
+                inputDataTurnamen(x);
+                p = createElemenTurnamen(x);
+                insertAfterTurnamen(L, prec, p);
+            }else{
+                cout <<"Turnamen tidak ditemukan";
+            }
+        }else if (pilih == 4) {
+            deleteFirstTurnamen(L, p);
+            if (p != nullptr){
+                cout << "Turnamen dengan nama: "<< p->info.namaTurnamen << " berhasil dihapus";
+            }else{
+                cout << "List kosong";
+            }
+        }else if (pilih == 5) {
+            deleteLastTurnamen(L, p);
+            if (p != nullptr){
+                cout << "Turnamen dengan nama: "<< p->info.namaTurnamen << " berhasil dihapus";
+            }else{
+                cout << "List kosong";
+            }
+        }else if (pilih == 6) {
+            cout << "Delete setelah turnamen bernama: ";
+            cin >> nama;
+            prec = searchTurnamenByNama(L, nama);
+            deleteAfterTurnamen(L, prec, p);
+            if (p != nullptr){
+                cout << "Turnamen dengan nama: "<< p->info.namaTurnamen <<" berhasil dihapus";
+            }else{
+                cout << "Tidak ada turnamen setelahnya";
+            }
+        }else if (pilih == 7) {
+            cout << "Cari tahun: ";
+            cin >> tahun;
+            p = searchTurnamenByTahun(L, tahun);
+            if (p != nullptr){
+                cout << "Ditemukan: " << p->info.namaTurnamen << endl;
+            }else{
+                cout << "Tidak ditemukan\\n";
+            }
+        }else if (pilih == 8) {
+            displayTurnamen(L);
+        }else if (pilih == 0) {
+            cout << "Kembali ke menu admin";
+        }else {
+            cout << "Pilihan tidak valid!";
+        }
+        cout << endl;
+    }
+}
+void menuPemain(ListTurnamen &L){
+    int pilih = 1;
+    string namaTurnamen, id, nama;
+    int score;
+    adrTurnamen T;
+    adrPemain P, prec;
+
+    while (pilih != 0) {
+        cout << "============================================" << endl;
+        cout << "|               MENU PEMAIN                |" << endl;
+        cout << "============================================" << endl;
+        cout << "| 1. Insert First Pemain                   |" << endl;
+        cout << "| 2. Insert Last Pemain                    |" << endl;
+        cout << "| 3. Insert After Pemain                   |" << endl;
+        cout << "| 4. Delete First Pemain                   |" << endl;
+        cout << "| 5. Delete Last Pemain                    |" << endl;
+        cout << "| 6. Delete After Pemain                   |" << endl;
+        cout << "| 7. Search Pemain                         |" << endl;
+        cout << "| 8. Print Pemain dalam Turnamen           |" << endl;
+        cout << "| 0. Kembali                               |" << endl;
+        cout << "============================================" << endl;
+        cout << "Masukkan Pilihan Anda: ";
+        cin >> pilih;
+
+        if (pilih == 1) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+            if (T != nullptr) {
+                cout << "ID Pemain   : "; cin >> id;
+                cout << "Nama Pemain : "; cin >> nama;
+                cout << "Score       : "; cin >> score;
+                P = createElmPemain(id, nama, score);
+                insertFirstPemain(T, P);
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 2) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                cout << "ID Pemain   : "; cin >> id;
+                cout << "Nama Pemain : "; cin >> nama;
+                cout << "Score       : "; cin >> score;
+                P = createElmPemain(id, nama, score);
+                insertLastPemain(T, P);
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 3) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                cout << "Insert setelah ID Pemain: ";
+                cin >> id;
+                prec = findPemain(T, id);
+
+                if (prec != nullptr) {
+                    cout << "ID Pemain   : "; cin >> id;
+                    cout << "Nama Pemain : "; cin >> nama;
+                    cout << "Score       : "; cin >> score;
+                    P = createElmPemain(id, nama, score);
+                    insertAfterPemain(T, prec, P);
+                } else {
+                    cout << "Pemain tidak ditemukan\n";
+                }
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 4) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                deleteFirstPemain(T, P);
+                if (P != nullptr)
+                    cout << "Pemain pertama berhasil dihapus\n";
+                else
+                    cout << "Belum ada pemain\n";
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 5) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                deleteLastPemain(T, P);
+                if (P != nullptr)
+                    cout << "Pemain terakhir berhasil dihapus\n";
+                else
+                    cout << "Belum ada pemain\n";
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 6) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                cout << "Delete setelah ID Pemain: ";
+                cin >> id;
+                prec = findPemain(T, id);
+                if (prec != nullptr) {
+                    deleteAfterPemain(T, prec, P);
+                    if (P != nullptr)
+                        cout << "Pemain berhasil dihapus\n";
+                    else
+                        cout << "Tidak ada pemain setelahnya\n";
+                } else {
+                    cout << "Pemain tidak ditemukan\n";
+                }
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+
+        // ================= SEARCH & PRINT =================
+        else if (pilih == 7) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                cout << "Cari ID Pemain: ";
+                cin >> id;
+                P = findPemain(T, id);
+                if (P != nullptr) {
+                    cout << "ID    : " << P->idPemain << endl;
+                    cout << "Nama  : " << P->nama << endl;
+                    cout << "Score : " << P->score << endl;
+                } else {
+                    cout << "Pemain tidak ditemukan\n";
+                }
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+        else if (pilih == 8) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+            if (T != nullptr)
+                printPemain(T);
+            else
+                cout << "Turnamen tidak ditemukan\n";
+        }
+        else if (pilih == 0) {
+            cout << "Kembali ke menu admin\n";
+        }
+        else {
+            cout << "Pilihan tidak valid!\n";
+        }
+
+        cout << endl;
+    }
+}
+void menuUser(ListTurnamen &L){
+    int pilih = 1;
+    string namaTurnamen, id;
+    int tahun;
+    adrTurnamen T;
+    adrPemain P;
+
+    while (pilih != 0) {
+        cout << "============================================" << endl;
+        cout << "|                MENU USER                 |" << endl;
+        cout << "============================================" << endl;
+        cout << "| 1. Lihat Semua Turnamen                  |" << endl;
+        cout << "| 2. Cari Turnamen (berdasarkan tahun)     |" << endl;
+        cout << "| 3. Lihat Pemain dalam Turnamen           |" << endl;
+        cout << "| 4. Cari Pemain dalam Turnamen            |" << endl;
+        cout << "| 0. Kembali                               |" << endl;
+        cout << "============================================" << endl;
+        cout << "Masukkan Pilihan Anda: ";
+        cin >> pilih;
+
+        // ================= VIEW =================
+        if (pilih == 1) {
+            displayTurnamen(L);
+        }
+
+        else if (pilih == 2) {
+            cout << "Masukkan Tahun Turnamen: ";
+            cin >> tahun;
+            T = searchTurnamenByTahun(L, tahun);
+            if (T != nullptr) {
+                cout << "Turnamen ditemukan:" << endl;
+                cout << "Nama     : " << T->info.namaTurnamen << endl;
+                cout << "Lokasi   : " << T->info.lokasi << endl;
+                cout << "Kategori : " << T->info.kategori << endl;
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+
+        else if (pilih == 3) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+            if (T != nullptr) {
+                printPemain(T);
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+
+        else if (pilih == 4) {
+            cout << "Nama Turnamen: ";
+            cin >> namaTurnamen;
+            T = searchTurnamenByNama(L, namaTurnamen);
+
+            if (T != nullptr) {
+                cout << "Cari ID Pemain: ";
+                cin >> id;
+                P = findPemain(T, id);
+                if (P != nullptr) {
+                    cout << "ID    : " << P->idPemain << endl;
+                    cout << "Nama  : " << P->nama << endl;
+                    cout << "Score : " << P->score << endl;
+                } else {
+                    cout << "Pemain tidak ditemukan\n";
+                }
+            } else {
+                cout << "Turnamen tidak ditemukan\n";
+            }
+        }
+
+        else if (pilih == 0) {
+            cout << "Kembali ke main menu\n";
+        }
+
+        else {
+            cout << "Pilihan tidak valid!\n";
+        }
+
+        cout << endl;
+    }
+}
+void inputDataTurnamen(Turnamen &x) {
+    cout << "Nama Turnamen   : ";
+    cin >> x.namaTurnamen;
+    cout << "Lokasi          : ";
+    cin >> x.lokasi;
+    cout << "Tanggal Mulai   : ";
+    cin >> x.tanggalMulai;
+    cout << "Tanggal Selesai : ";
+    cin >> x.tanggalSelesai;
+    cout << "Tahun           : ";
+    cin >> x.tahun;
+    cout << "Kategori        : ";
+    cin >> x.kategori;
+}
